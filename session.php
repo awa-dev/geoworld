3edb5964b98833eabbd709357d0a8776c4a14e2f
awa <77786970+awa-dev@users.noreply.github.com>
2021-04-23 13:38:23 +0200

    modif awa
    modif awa

---
+++ session.php
@@ -0,0 +1,28 @@
+<?php
+    require_once('inc\manager-db.php');
+    // on teste si nos variables sont définies et remplies
+    if (isset($_POST['login']) && isset($_POST['pwd']) && !empty($_POST['login']) && !
+        empty($_POST['login'])) {
+        // on appele la fonction getAuthentification en lui passant en paramètre le l'identifiant et password
+        //la fonction retourne les caractéristiques du salaries si il est connu sinon elle retourne false
+        $result = getAuthentification($_POST['login'], $_POST['pwd']);
+        print_r($result);
+        // si le résulat n'est pas false
+        if ($result) {
+    // on la démarre la session
+            session_start();
+    // on enregistre les paramètres de notre visiteur comme variables de session
+            $_SESSION['nom'] = $result->nom;
+            $_SESSION['role'] = $result->role;
+    // on redirige notre visiteur vers une page de notre section membre
+            header('location: header.php?continent=$continent->Continent');
+
+        } //si le résultat est false on redirige vers la page d'authentification
+        else {
+            header('location: login.php');
+        }
+    } //si nos variables ne sont pas renseignées on redirige vers la page d'authentification
+    else {
+        header('location: login.php');
+    }
+?>
